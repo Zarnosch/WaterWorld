2d51b8195d9393ef6e1c62865779d70f715ee982
YsenGrimm <kloeten.wars@googlemail.com>
2017-06-30 22:26:22 +0200

    camera movement and selection basics

---
+++ Waterworld/Assets/Scripts/CameraMouseMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraMouseMovement : MonoBehaviour {
+
+	public int   MovementZone = 150;
+	public float MoveOffset = 0.15f;
+
+	private Vector3 mousePos;
+	
+	void Update () {
+		mousePos = Input.mousePosition;
+		// left
+		if (mousePos.x > 0 && mousePos.x < MovementZone) {
+			transform.position = new Vector3(
+				transform.position.x - MoveOffset, 
+				transform.position.y, 
+				transform.position.z);
+		}
+
+		// down
+		if (mousePos.y > 0 && mousePos.y < MovementZone) {
+			transform.position = new Vector3(
+				transform.position.x, 
+				transform.position.y - MoveOffset, 
+				transform.position.z);
+		}
+
+		// rigth
+		if (mousePos.x > Screen.width - MovementZone && mousePos.x < Screen.width) {
+			transform.position = new Vector3(
+				transform.position.x + MoveOffset, 
+				transform.position.y, 
+				transform.position.z);
+		}
+
+		// up
+		if (mousePos.y > Screen.height - MovementZone && mousePos.y < Screen.height) {
+			transform.position = new Vector3(
+				transform.position.x, 
+				transform.position.y + MoveOffset, 
+				transform.position.z);
+		}
+	}
+}
---
+++ Waterworld/Assets/Scripts/MouseSelection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSelection : MonoBehaviour {
+
+	Ray mouseRay;
+	
+	void Update () {
+		// only interested if left mouse button
+		if (!Input.GetMouseButtonDown(0)) { return; }
+		RaycastHit hit;
+
+		mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Physics.Raycast(mouseRay, out hit);
+
+		// nothing hit
+		if (hit.distance == 0) { return; }
+
+		Debug.Log(hit.transform.gameObject.name);
+	}
+}
