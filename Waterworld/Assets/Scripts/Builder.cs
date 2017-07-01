c020cc55f2bf1d0d54297f9bede2df43c02a155d
YsenGrimm <kloeten.wars@googlemail.com>
2017-07-01 21:21:00 +0200

    stuff

--- Waterworld/Assets/Scripts/Builder.cs
+++ Waterworld/Assets/Scripts/Builder.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System;
 using UnityEngine;
 
 public enum Building {
@@ -21,12 +23,13 @@ public enum Building {
 
 public class Builder : MonoBehaviour {
 
-	public RaftBehaviour Raft;
+	public RaftBehaviour      Raft;
+
+	public List<BuildingType> Buildings;
 
 	private Building _selectedBuilding;
 	private bool     _isBuilding;
-
-	Ray mouseRay;
+	private Ray      _mouseRay;
 
 	public void Build(Building _building) {
 		_selectedBuilding = _building;
@@ -36,19 +39,54 @@ public class Builder : MonoBehaviour {
 	void Update() {
 		// only interested if left mouse button
 		if (!Input.GetMouseButtonDown(0)) { return; }
+
 		RaycastHit hit;
-        mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-		Physics.Raycast(mouseRay, out hit);
+        _mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Physics.Raycast(_mouseRay, out hit);
 
 		// nothing hit
 		if (hit.distance == 0) { return; }
 
-        if (hit.transform.gameObject.GetComponent<BuildingType>().Type == Building.Raft) {
+		if (!_isBuilding) { return; }
+		Building type = Building.NONE;
+		try {
+			type = hit.transform.gameObject.GetComponent<BuildingType>().Type;
+		} catch (System.NullReferenceException _e) {
+			Debug.Log("Selecting building without 'BuildingType' something went wrong.");
+			Debug.Log(_e.StackTrace);
+			return;
+		}
+
+        if (type == Building.Raft) {
             Vec2i raftPos = Raft.LightMapPosToHeightmap(hit.lightmapCoord);
 			if (Raft.Build(raftPos, _selectedBuilding)) {
+				placeBuilding(_selectedBuilding, new Vector2(raftPos.X + 0.5f, raftPos.Y + 0.5f));
 				_isBuilding       = false;
 				_selectedBuilding = Building.NONE;
 			}
         }
+	}
+
+	void placeBuilding(Building _building, Vector2 _pos) {
+		Vector3 placePos = new Vector3(_pos.x, Raft.transform.position.y, _pos.y);
+
+		BuildingType buildingPrefab = null;
+		// var buildingPrefab = (from b in Buildings where b.Type == _building select b).ElementAt(0);
+		foreach (var b in Buildings) {
+			if (b.Type == _building) {
+				buildingPrefab = b;
+			}
+		}
+		Debug.Log(buildingPrefab);
+
+		if (buildingPrefab == null) { return; }
+
+		switch (_building) {
+			case Building.Fisherman:
+				BuildingType building = Instantiate(buildingPrefab);
+				// building.transform.SetParent(transform);
+				building.transform.position = placePos;
+				break;
+		}
 	}
 }
